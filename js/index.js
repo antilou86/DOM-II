96c502f7f2813a0c837791830042c78c8c908658
Lou <antilou86@gmail.com>
2019-06-18 15:38:07 -0400

    WIP: added alert to links on click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+let navi= document.querySelector('.nav');
+
+navi.addEventListener('click', () => {
+    alert('Oh, NOEZ!! the links are broken. SOMEONE CALL THE DEVS!');
+});
+
+
+let body = document.querySelector('body');
+
+console.log(body);
+
+body.addEventListener('dblclick', () => {
+    body.style.backgroundColor = 'grey';
+    navi.style.backgroundColor = 'grey';
+    document.querySelector('header').style.backgroundColor= 'grey';
+    body.style.color = 'white';
+    document.querySelector('header').style.color = 'white';
+    alert('night mode initiated,')
+});
