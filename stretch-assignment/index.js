d6a1887e5e78e091e344773b192ba61106a67b8a
Lou <antilou86@gmail.com>
2019-07-09 15:38:56 -0400

    stretch: first commit- added each block elements to a variable for easier manipulation

---
+++ stretch-assignment/index.js
@@ -0,0 +1,8 @@
+const blocks = document.querySelectorAll('.block');
+
+const redBlock = document.querySelector('.block--red');
+const blueBlock = document.querySelector('.block--blue');
+const greenBlock = document.querySelector('.block--green');
+const pinkBlock = document.querySelector('.block--pink');
+const grayBlock = document.querySelector('.block--gray');
+
